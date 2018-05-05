e28e3923c5538ccf6262e19edcd1cacff77b5dfb
Anushri Adhia <anushri.adhia@gmail.com>
2018-05-05 15:42:57 -0400

    Implemented serializer registry

--- src/serializer/SerializerRegistry.java
+++ src/serializer/SerializerRegistry.java
@@ -1,5 +1,27 @@
 package serializer;
 
+import java.util.HashMap;
+
+import util.annotations.Tags;
+//@Tags({SERIALIZER_REGISTRY})
+
 public class SerializerRegistry {
+	
+	static HashMap<Class, ValueSerializer> serializerMap;
+	
+	public SerializerRegistry() {
+		
+		serializerMap = new HashMap<Class, ValueSerializer>();
+		
+	}
+	
+	
+	public static void registerValueSerializer (Class aClass, ValueSerializer serializer) {
+		serializerMap.put(aClass, serializer);
+	}
+	
+	public static ValueSerializer getValueSerializer(Class aClass) {
+		return serializerMap.get(aClass);
+	}
 
 }
