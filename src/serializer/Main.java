77edad90ee9d200b8d407ba1b5566c37a4748127
Paresh Adhia <paresh_adhia@yahoo.com>
2018-05-06 22:05:11 -0400

    Add ability to get/put arbitrary length String

--- src/serializer/Main.java
+++ src/serializer/Main.java
@@ -1,12 +1,23 @@
 package serializer;
 
+import java.io.StreamCorruptedException;
+
 public class Main {
 
-	public static void main(String[] args) {
-		MutableString ms = new MutableString();
-		
-		ms.append(String.format("%010d", 5));
-		ms.append("hello world");
-		System.out.println(ms);
+	public static void main(String[] args) throws StreamCorruptedException {
+		TextBuffer buf = new TextBuffer();
+
+		buf.put("Hello World");  // append a string
+		buf.put(1234);           // append an integer
+		System.out.println(buf); // print current buffer
+
+		String s;                
+
+		s = buf.get();           // retrieve first object => should be "Hello World"
+		System.out.println(s);
+
+		s = buf.get();           // retrieve second object => should be toString() of 1234
+		System.out.println(s);   
+		System.out.println(Integer.valueOf(s));
 	}
 }
